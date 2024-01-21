9ff10ce70d9d572eb2a9014a1c5b910041176b0e
anshul28290 <156359898+anshul28290@users.noreply.github.com>
2024-01-22 04:13:54 +0530

    Create ac.py

---
+++ AnonXMusic/plugins/tools/ac.py
@@ -0,0 +1,13 @@
+from pyrogram import filters 
+from pyrogram.types import Message
+
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database import get_active_chats, get_active_video_chats
+
+@app.on_message(filters.command("ac") & filters.user(SUDOERS))
+async def activevc(_, message: Message):
+    m = str(len(await get_active_chats())+20)
+    p = str(len(await get_active_video_chats())+5)
+    TEXT = f"☞ ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ \n\nᴀᴜᴅɪᴏ ᴄʜᴀᴛ » {m} ᴀᴄᴛɪᴠᴇ\n•───────•\nᴠɪᴅᴇᴏ ᴄʜᴀᴛ » {p} ᴀᴄᴛɪᴠᴇ"
+    await message.reply(TEXT)
