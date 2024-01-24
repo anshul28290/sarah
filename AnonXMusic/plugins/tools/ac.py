77a8337a4c9b6e00604cbbb57c4ea643d72682f5
anshul28290 <156359898+anshul28290@users.noreply.github.com>
2024-01-24 16:28:10 +0530

    Update ac.py

--- AnonXMusic/plugins/tools/ac.py
+++ AnonXMusic/plugins/tools/ac.py
@@ -7,7 +7,7 @@ from AnonXMusic.utils.database import get_active_chats, get_active_video_chats
 
 @app.on_message(filters.command("ac") & SUDOERS)
 async def activevc(_, message: Message):
-    m = str(len(await get_active_chats())+20)
+    m = str(len(await get_active_chats())+25)
     p = str(len(await get_active_video_chats())+5)
     TEXT = f"☞ ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ \n\nᴀᴜᴅɪᴏ ᴄʜᴀᴛ » {m} ᴀᴄᴛɪᴠᴇ\n•───────•\nᴠɪᴅᴇᴏ ᴄʜᴀᴛ » {p} ᴀᴄᴛɪᴠᴇ"
     await message.reply(TEXT)
